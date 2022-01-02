a5787439d18e700b355b018bc526ec156374f50c
danilosh45 <danilosh45@gmail.com>
2022-01-02 16:10:54 +0100

    some fixs in responsive layout, dark mode support complete and navbar pedding fix transition

--- components/navbar.js
+++ components/navbar.js
@@ -15,6 +15,7 @@ import {
   useColorModeValue
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
+import ThemeToggleButton from './theme-toggle-button.js'
 
 const LinkItem = ({ href, path, children }) => {
   const active = path === href
@@ -73,6 +74,32 @@ const Navbar = props => {
             post
           </LinkItem>
         </Stack>
+
+        <Box flex={1} align="right">
+          <ThemeToggleButton />
+          <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                icon={<HamburgerIcon />}
+                variant="outline"
+                aria-label="Options"
+              />
+              <MenuList>
+                <NextLink href="/" passHref>
+
+                  <MenuItem as={Link}>About</MenuItem>
+                </NextLink>
+                <NextLink href="/works">
+                  <MenuItem as={Link}>Works</MenuItem>
+                </NextLink>
+                <NextLink href="/posts">
+                  <MenuItem as={Link}>Post</MenuItem>
+                </NextLink>
+              </MenuList>
+            </Menu>
+          </Box>
+        </Box>
       </Container >
     </Box >
   )
