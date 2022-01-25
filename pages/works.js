ccf2ddd44e825bb8ac926125bd57938db8f25bf3
Danilo Altamirano <danilosh45@gmail.com>
2022-01-25 18:37:54 +0100

    creating and update works page and adding static media images and more stuff(media)

--- pages/works.js
+++ pages/works.js
@@ -2,9 +2,9 @@ import { Container, SimpleGrid, Heading, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
-
 import thumbIndrop from '../public/images/works/inkdrop_eyecatch.png'
 import thumbWalknote from '../public/images/works/walknote_eyecatch.png'
+
 
 const Works = () => {
   return (
@@ -15,15 +15,23 @@ const Works = () => {
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
-          <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbIndrop}>
-            A project by danilo comming soom
+          <WorkGridItem id="inkdrop" title="HeyCreativos" thumbnail={thumbIndrop}>
+            Build backend and deploy for Comercial website
           </WorkGridItem>
         </Section>
         <Section>
-          <WorkGridItem id="walknote" title="walknote" thumbnail={thumbWalknote}>
+          <WorkGridItem id="walknote" title="Mimi" thumbnail={thumbWalknote}>
             A simple bot for discord for assitance and more
           </WorkGridItem>
         </Section>
+        <Section>
+          <WorkGridItem id="walknote" title="Dash dashboard" thumbnail={thumbWalknote}>
+            Web scraping for dev
+          </WorkGridItem>
+        </Section>
+
+        <Divider />
+
       </SimpleGrid>
     </Container>
   )
